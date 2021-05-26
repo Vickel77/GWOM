f1aa81aacb4e067f2d83835c56290bbe63c5c969
vickel <kelechialigwo77@gmail.com>
2021-05-26 08:13:40 +0100

    blog details

---
+++ pages/blog/details.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Details = styled(({className})=>{
+  return(
+    <div className={className}>
+    </div>
+  )
+})`
+
+`
+
+export default Details;
+
